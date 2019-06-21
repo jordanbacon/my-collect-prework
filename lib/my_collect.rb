10f4a7004b8d82e1ade19d374bda1a29e5542158
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 07:47:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
-    i = 0
-    array = []
-    while i < collection.length
-    array << yield(collection[i])
-      i = i + 1
+  i = 0
+  array = []
+  while i < collection.length
+  array << yield(collection[i])
+    i = i + 1
     end
   array
 end
